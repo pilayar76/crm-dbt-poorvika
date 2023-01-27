5e87fedba7426e2dff1e684b72ec29db17fa7da3
root <rajakrishna76@gmail.com>
2023-01-27 02:56:35 +0000

    Adding Related Products

--- models/enquires/enquires_transactions.sql
+++ models/enquires/enquires_transactions.sql
@@ -1,6 +1,7 @@
 with transactions as (
 
-        select * from {{ ref('stg_transactions') }}
+	select _id, item_object from (SELECT _id, arr.position,arr.item_object FROM enquires,
+	jsonb_array_elements(transactions) with ordinality arr(item_object, position)) as a where item_object->'additional' is  null
 
 ),
 
--- models/stg_transactions.sql
+++
@@ -1,3 +0,0 @@
-select _id, item_object from (SELECT _id, arr.position,arr.item_object
-FROM enquires,
-jsonb_array_elements(transactions) with ordinality arr(item_object, position)) as a where item_object->'additional' is  null
